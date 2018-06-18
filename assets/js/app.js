2731ede340cd09ef6f13dbdfed1743b09055644d
AriellaRogueN <maitilydevidasi@gmail.com>
2018-06-18 18:22:26 -0400

    buscar gift con fetch

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+const inputText =document.querySelector("input")
+const containerImage =document.getElementById("elementos")
+
+inputText.addEventListener("keypress", (event)=>
+{
+  let key = event.which || event.keyCode;
+  if (key ===13 )
+  {
+    let gif = inputText.value;
+    inputText.value = "";
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=PzBiUI6sCbqrLCFDOGArGWBCtfoauzYf&q=${gif}&limit=25&offset=0&rating=G&lang=en`)
+    .then(response => response.json())
+    .then(data => {
+      renderInfo(data);
+    })
+  }
+})
+const renderInfo = data =>{
+  data.data.forEach(element => {
+  let gifImage = element.images.downsized.url;
+  containerImage.innerHTML += `<img src="${gifImage}">`; 
+  
+});
+}
